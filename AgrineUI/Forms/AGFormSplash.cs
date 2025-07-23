9e4dc2b9c3b95ea5df2ed8c7272b4d477c5fdcb9
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-24 02:12:27 +0330

    - Add AGFormSplash To Forms

---
+++ AgrineUI/Forms/AGFormSplash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgrineUI.Forms
+{
+    public partial class AGFormSplash : AgrineUI.Forms.AGForm
+    {
+        public AGFormSplash()
+        {
+            InitializeComponent();
+        }
+    }
+}
