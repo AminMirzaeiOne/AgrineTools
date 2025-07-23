511e13e8fc79178005a3635ba27cc6aa5f6414d4
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-24 02:32:47 +0330

    - Add AgrineLogo Shape In Shapes Folder

---
+++ AgrineUI/Shapes/AgrineLogo.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace AgrineUI.Shapes
+{
+    partial class AgrineLogo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AgrineLogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "AgrineLogo";
+            this.Size = new System.Drawing.Size(321, 217);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
