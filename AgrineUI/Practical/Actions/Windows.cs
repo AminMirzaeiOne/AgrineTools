ad693792199ea70b602ca7115964a903bbedc4bb
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-26 19:55:37 +0330

    - Add Shutdown Action Method In Windows Static Class

---
+++ AgrineUI/Practical/Actions/Windows.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgrineUI.Practical.Actions
+{
+    public static class Windows
+    {
+        public enum OperationTypes
+        {
+            None, Shutdown, Restart, Sleep, Look
+        }
+
+        private static AgrineUI.Practical.Actions.Windows.OperationTypes operation = OperationTypes.None;
+
+        public static AgrineUI.Practical.Actions.Windows.OperationTypes Operation
+        {
+            get { return operation; }
+            set
+            {
+                operation = value;
+                switch (value)
+                {
+                    case OperationTypes.Shutdown:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Windows operating system shutdown action
+        /// </summary>
+        /// <param name="delay">Windows shutdown delay value</param>
+        public static void Shutdown(byte delay = 5)
+        {
+            Process.Start($"shutdown", "/s /t " + delay);
+        }
+
+    }
+}
