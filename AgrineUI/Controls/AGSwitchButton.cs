394f42fc0e6d448f8d3888b50e05042d94ddb03c
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-01 04:44:07 +0330

    - Remove Graphics Folder To Practical Folder

--- AgrineUI/Controls/AGSwitchButton.cs
+++ AgrineUI/Controls/AGSwitchButton.cs
@@ -1,13 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace AgrineUI.Controls
 {
-    public class AGSwitchButton
+    public class AGSwitchButton : System.Windows.Forms.Control
     {
 
+        private bool isChecked = false;
+
+        [Category("Behavior")]
+        public bool Checked
+        {
+            get { return isChecked; }
+            set
+            {
+                isChecked = value;
+                Invalidate(); // Redraw
+                CheckedChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        [Category("Behavior")]
+        public event EventHandler CheckedChanged;
+
+        public AGSwitchButton()
+        {
+            this.DoubleBuffered = true;
+            this.Size = new Size(60, 30);
+            this.Cursor = Cursors.Hand;
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            Checked = !Checked;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+
+            using (SolidBrush backgroundBrush = new SolidBrush(this.BackColor))
+            {
+                System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+                int radius = Height / 2;
+                path.AddArc(0, 0, radius, radius, 180, 90);
+                path.AddArc(Width - radius, 0, radius, radius, 270, 90);
+                path.AddArc(Width - radius, Height - radius, radius, radius, 0, 90);
+                path.AddArc(0, Height - radius, radius, radius, 90, 90);
+                path.CloseFigure();
+                e.Graphics.FillPath(backgroundBrush, path);
+            }
+
+        }
     }
+
+
+
 }
--- AgrineUI/Practical/Graphics/AGRadius.cs
+++ AgrineUI/Practical/Graphics/AGRadius.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AgrineUI.Graphics
+namespace AgrineUI.Practical.Graphics
 {
     public static class AGRadius
     {
