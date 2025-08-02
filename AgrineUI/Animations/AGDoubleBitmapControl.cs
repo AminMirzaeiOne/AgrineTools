27fed97f1f01339cef8dcbfe6ee70cfd076b8845
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-02 08:06:16 +0330

    - Update and Complete AGDoubleBitmapControl Control Class

---
+++ AgrineUI/Animations/AGDoubleBitmapControl.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgrineUI.Animations
+{
+    public partial class AGDoubleBitmapControl : System.Windows.Forms.Control, IFakeControl
+    {
+        Bitmap bgBmp;
+        Bitmap frame;
+
+        Bitmap IFakeControl.BgBmp { get { return this.bgBmp; } set { this.bgBmp = value; } }
+        Bitmap IFakeControl.Frame { get { return this.frame; } set { this.frame = value; } }
+
+
+        public event EventHandler<TransfromNeededEventArg> TransfromNeeded;
+        public event EventHandler<PaintEventArgs> FramePainted;
+        public event EventHandler<PaintEventArgs> FramePainting;
+
+        public AGDoubleBitmapControl()
+        {
+            this.InitializeComponent();
+
+            Visible = false;
+            SetStyle(ControlStyles.Selectable, false);
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var gr = e.Graphics;
+
+            OnFramePainting(e);
+
+            try
+            {
+                gr.DrawImage(bgBmp, 0, 0);
+                if (frame != null)
+                {
+                    var ea = new TransfromNeededEventArg() { ClientRectangle = new Rectangle(0, 0, this.Width, this.Height) };
+                    ea.ClipRectangle = ea.ClientRectangle;
+                    OnTransfromNeeded(ea);
+                    gr.SetClip(ea.ClipRectangle);
+                    gr.Transform = ea.Matrix;
+                    gr.DrawImage(frame, 0, 0);
+                }
+            }
+            catch { }
+
+            OnFramePainted(e);
+        }
+
+        private void OnTransfromNeeded(TransfromNeededEventArg ea)
+        {
+            if (TransfromNeeded != null)
+                TransfromNeeded(this, ea);
+        }
+
+        protected virtual void OnFramePainting(PaintEventArgs e)
+        {
+            if (FramePainting != null)
+                FramePainting(this, e);
+        }
+
+        protected virtual void OnFramePainted(PaintEventArgs e)
+        {
+            if (FramePainted != null)
+                FramePainted(this, e);
+        }
+
+        public void InitParent(Control control, Padding padding)
+        {
+            Parent = control.Parent;
+            int i = 0;
+            if (control.Parent != null)
+            {
+                i = control.Parent.Controls.GetChildIndex(control);
+                control.Parent.Controls.SetChildIndex(this, i);
+            }
+            Bounds = new Rectangle(
+                control.Left - padding.Left,
+                control.Top - padding.Top,
+                control.Size.Width + padding.Left + padding.Right,
+                control.Size.Height + padding.Top + padding.Bottom);
+        }
+
+    }
+
+    partial class AGDoubleBitmapControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+
+    }
+
+    public interface IFakeControl
+    {
+        Bitmap BgBmp { get; set; }
+        Bitmap Frame { get; set; }
+        event EventHandler<TransfromNeededEventArg> TransfromNeeded;
+        event EventHandler<PaintEventArgs> FramePainting;
+        event EventHandler<PaintEventArgs> FramePainted;
+        void InitParent(Control animatedControl, Padding padding);
+    }
+}
