1bba3a6255c6a0080b6c39675a0b058ee2ea32df
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-06 00:12:40 +0330

    - Add IAGControlActoin and IAGControlSound Interface

---
+++ AgrineUI/Abstracts/AGButtonBase.cs
@@ -0,0 +1,18 @@
+﻿using AgrineUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgrineUI.Abstracts
+{
+    [ToolboxItem(false)]
+    public class AGButtonBase : System.Windows.Forms.Button, IAGControlTheme
+    {
+        public Color Palette { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DarkMode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
