2b9b1a4231feeb06be71442356b456d839f94a43
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-10 01:31:56 +0330

    - Add and Update AppDefinitions Class Base Code

---
+++ AgrineCore/Actions/AppsDefinitions.cs
@@ -0,0 +1,59 @@
+﻿namespace AgrineCore.Actions
+{
+    public class AppsDefinitions
+    {
+        public enum AppsActionTypes
+        {
+            None, Start, Exit
+        }
+
+        public enum AppsWindowStatus
+        {
+            Normal, Maximize, Minimize
+        }
+
+        public AppsActionTypes ActionType { get; set; } = AppsActionTypes.None;
+        public AppsWindowStatus AppWindowStatus { get; set; } = AppsWindowStatus.Normal;
+        public int ActionWait { get; set; } = 1;
+
+        public struct TopApps
+        {
+            public enum Windows
+            {
+                None, Notepad, Wordpad, Calculator, ControlPanel, Camera, Paint, Explorer, Terminal, PowerShell
+            }
+
+            public enum Microsoft
+            {
+                Mail, Maps, Calendar, Clock
+            }
+
+            public enum Office
+            {
+                Word, PowerPoint, Excel, Access,
+            }
+
+            public enum Adobe
+            {
+                Photoshop, Illustrator, XD, InDesign, Premiere, Dreamweaver, AfterEffects, Lightroom
+            }
+
+            public enum WebBrowser
+            {
+                Edge, Chrome, Firefox, Opera, Vivaldi, Brave
+            }
+
+            public enum CodeEditor
+            {
+                NotepadPlusPlus, VSCode, Atom
+            }
+
+            public enum IDE
+            {
+                QtCreator, VisualStudio, CLion
+            }
+        }
+
+    }
+}
+
