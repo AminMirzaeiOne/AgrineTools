115449f3f9e460ee7611c06547d6aadb3e471602
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-09 04:20:34 +0330

    - Update AudioConverter Class (Add ChangeAudioExtension Function)

--- AgrineCore/Converter/AudioConverter.cs
+++ AgrineCore/Converter/AudioConverter.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace AgrineCore.Converter
+namespace AgrineCore.Practical
 {
-    internal class AudioConverter
+    public static class AudioConverter
     {
+        /// <summary>
+        /// Note: Real audio format conversion requires external libs.
+        /// This method does a simple file copy with changed extension if valid.
+        /// </summary>
+        public static bool ChangeAudioExtension(string inputPath, string outputPath)
+        {
+            try
+            {
+                if (!File.Exists(inputPath))
+                    return false;
+
+                var validExtensions = new[] { ".mp3", ".wav", ".aac", ".flac", ".ogg" };
+                string inputExt = Path.GetExtension(inputPath).ToLower();
+                string outputExt = Path.GetExtension(outputPath).ToLower();
+
+                if (Array.IndexOf(validExtensions, inputExt) < 0 ||
+                    Array.IndexOf(validExtensions, outputExt) < 0)
+                    return false;
+
+                // Just copy file; this does NOT convert audio data format
+                File.Copy(inputPath, outputPath, overwrite: true);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
