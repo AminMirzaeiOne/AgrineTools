f37ccb785fce8ac459e044413db840679a5372f2
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-09 04:26:30 +0330

    - Update ZipManager Class Base Code

--- AgrineCore/Practical/ZipManager.cs
+++ AgrineCore/Practical/ZipManager.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
 
 namespace AgrineCore.Practical
 {
-    internal class ZipManager
+    public static class ZipManager
     {
+        public static bool CreateZip(string sourceFolder, string zipFilePath)
+        {
+            try
+            {
+                if (File.Exists(zipFilePath))
+                    File.Delete(zipFilePath);
+
+                ZipFile.CreateFromDirectory(sourceFolder, zipFilePath, CompressionLevel.Optimal, false);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool ExtractZip(string zipFilePath, string destinationFolder)
+        {
+            try
+            {
+                ZipFile.ExtractToDirectory(zipFilePath, destinationFolder);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
