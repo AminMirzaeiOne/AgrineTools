643529ce13191064904a66fb28cc00d95c173c1d
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-09 15:42:36 +0330

    - Add Definitions Class In Actions Folder

---
+++ AgrineCore/Actions/Definitions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgrineCore.Actions
+{
+    public class Definitions
+    {
+        
+    }
+}
