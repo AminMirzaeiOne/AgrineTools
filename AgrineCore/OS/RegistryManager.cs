778999f07ea793cce319473d55ed3f4b6e9de6c8
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-09 04:27:36 +0330

    - Update RegistryManager Base Code

--- AgrineCore/OS/RegistryManager.cs
+++ AgrineCore/OS/RegistryManager.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 
-namespace AgrineCore.OS
+namespace AgrineCore.Practical
 {
-    internal class RegistryManager
+    public static class RegistryManager
     {
+        public static object ReadValue(RegistryHive hive, string subKey, string valueName)
+        {
+            try
+            {
+                using (var baseKey = RegistryKey.OpenBaseKey(hive, RegistryView.Registry64))
+                using (var key = baseKey.OpenSubKey(subKey))
+                {
+                    if (key == null) return null;
+                    return key.GetValue(valueName);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static bool WriteValue(RegistryHive hive, string subKey, string valueName, object value)
+        {
+            try
+            {
+                using (var baseKey = RegistryKey.OpenBaseKey(hive, RegistryView.Registry64))
+                using (var key = baseKey.CreateSubKey(subKey))
+                {
+                    if (key == null) return false;
+                    key.SetValue(valueName, value);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteValue(RegistryHive hive, string subKey, string valueName)
+        {
+            try
+            {
+                using (var baseKey = RegistryKey.OpenBaseKey(hive, RegistryView.Registry64))
+                using (var key = baseKey.OpenSubKey(subKey, true))
+                {
+                    if (key == null) return false;
+                    key.DeleteValue(valueName);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
