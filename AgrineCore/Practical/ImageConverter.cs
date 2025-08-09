7a422d63ba0fde2cc2cefc65c0b154ba9f3b1178
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-08-09 04:17:59 +0330

    - Update ImageConverter Class Base Code

--- AgrineCore/Practical/ImageConverter.cs
+++ AgrineCore/Practical/ImageConverter.cs
@@ -1,12 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace AgrineCore.Practical
 {
-    internal class ImageConverter
+    public static class ImageConverter
     {
+        /// <summary>
+        /// Converts image file from one format to another.
+        /// Supported formats: jpg, png, bmp, gif, tiff
+        /// </summary>
+        public static bool ConvertImageFormat(string inputPath, string outputPath, ImageFormat outputFormat)
+        {
+            try
+            {
+                if (!File.Exists(inputPath))
+                    return false;
+
+                using (var image = Image.FromFile(inputPath))
+                {
+                    image.Save(outputPath, outputFormat);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Helper to get ImageFormat by extension
+        /// </summary>
+        public static ImageFormat GetImageFormat(string extension)
+        {
+            if (string.IsNullOrEmpty(extension))
+                return null;
+
+            extension = extension.ToLower().TrimStart('.');
+
+            switch (extension)
+            {
+                case "jpg":
+                case "jpeg":
+                    return ImageFormat.Jpeg;
+                case "png":
+                    return ImageFormat.Png;
+                case "bmp":
+                    return ImageFormat.Bmp;
+                case "gif":
+                    return ImageFormat.Gif;
+                case "tiff":
+                case "tif":
+                    return ImageFormat.Tiff;
+                default:
+                    return null;
+            }
+        }
     }
 }
